db339ebfe5c5123caa9acf3c65ba8a8532c091a5
heigo <heigo.tiidemaa@gmail.com>
2014-11-14 14:57:01 +0200

    Isikukoodi generaator koos kontrollnumbri validaatoriga

--- clippy.py
+++ clippy.py
@@ -2,7 +2,7 @@ __author__ = 'heigo'
 
 import subprocess, sys
 
-result = subprocess.check_call('C:\python34\python Isikukood_kontrollnumber.pya | clip', shell=True)
+result = subprocess.check_call('C:\python34\python Isikukood_kontrollnumber.py | clip', shell=True)
 # print(result)
 sys.exit(result)
 
